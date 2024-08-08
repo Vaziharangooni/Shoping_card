5b9a19ec1c38a30abee371dcf522b1aa68959e66
Vaziharangooni <154910721+Vaziharangooni@users.noreply.github.com>
2024-08-08 23:01:35 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+
+/* Footer */
+footer {
+    background:black;
+    color: white;
+    text-align: center;
+    padding: 15px 0;
+    border-radius: 0 0 15px 15px;
+    margin-top: 50px;
+    font-weight: 500;
+    letter-spacing: 1px;
+}
+</style>
+</head>
+<body>
+    <footer>
+        <p>&copy; 2024 Mood-Based Playlist Generator.All Rights Reserved. Develope By ~Vaziha Rangooni</p>
+    </footer>
+</body>
+</html>
